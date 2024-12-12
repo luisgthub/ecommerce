b5f284c63f748285789fad1730eedb1d3defa51c
luisgthub <luiamaral2@gmail.com>
2024-12-12 12:55:09 -0300

    first config

--- apps/ecomm/src/app/app.component.ts
+++ apps/ecomm/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { FaConfig, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecomm';
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit():void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome(){
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ apps/ecomm/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,5 @@
+import { faUser, IconDefinition } from "@fortawesome/free-regular-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+    faUser
+];
