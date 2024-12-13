dfa098b51234a13cb6d80a248d135950b796848d
luisgthub <luiamaral2@gmail.com>
2024-12-12 23:17:14 -0300

    first front-end page created

---
+++ apps/ecomm/src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule,FaIconComponent],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {}
